bc3730ff1242a81a5eb7d298ed9b0641626651fc
Samuel DeSantis <samuel.i.desantis@gmail.com>
2021-01-03 22:05:53 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -8,55 +8,56 @@ class MusicControllerLibrary
     input = ""
 
     while input != "exit"
-    puts "Welcome to your music library!"
-    puts "To list all of your songs, enter 'list songs'."
-    puts "To list all of the artists in your library, enter 'list artists'."
-    puts "To list all of the genres in your library, enter 'list genres'."
-    puts "To list all of the songs by a particular artist, enter 'list artist'."
-    puts "To list all of the songs of a particular genre, enter 'list genre'."
-    puts "To play a song, enter 'play song'."
-    puts "To quit, type 'exit'."
-    puts "What would you like to do?"
+      puts "Welcome to your music library!"
+      puts "To list all of your songs, enter 'list songs'."
+      puts "To list all of the artists in your library, enter 'list artists'."
+      puts "To list all of the genres in your library, enter 'list genres'."
+      puts "To list all of the songs by a particular artist, enter 'list artist'."
+      puts "To list all of the songs of a particular genre, enter 'list genre'."
+      puts "To play a song, enter 'play song'."
+      puts "To quit, type 'exit'."
+      puts "What would you like to do?"
 
-    input = gets.strip
+      input = gets.strip
 
-    case input
-    when 'list songs'
-      list_songs
-    when 'list artists'
-      list_artists
-    when 'list genres'
-      list_genres
-    when 'list artist'
-      list_songs_by_artists
-    when 'list genre'
-      list_songs_by_genre
-    when 'play song'
-      play_songs
+      case input
+      when 'list songs'
+        list_songs
+      when 'list artists'
+        list_artists
+      when 'list genres'
+        list_genres
+      when 'list artist'
+        list_songs_by_artists
+      when 'list genre'
+        list_songs_by_genre
+      when 'play song'
+        play_songs
+      end
     end
   end
-end
 
-def list_songs
+  def list_songs
 
-end
+  end
 
-def list_artists
+  def list_artists
 
-end
+  end
 
-def list_genres
+  def list_genres
 
-end
+  end
 
-def list_songs_by_artists
+  def list_songs_by_artists
 
-end
+  end
 
-def list_songs_by_genre
+  def list_songs_by_genre
 
-end
+  end
 
-def play_songs
+  def play_songs
 
+  end
 end
