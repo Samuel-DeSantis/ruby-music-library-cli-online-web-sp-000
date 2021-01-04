44d82a237ffbb662a54de3a33b539b51b8f2a6ba
Samuel DeSantis <samuel.i.desantis@gmail.com>
2021-01-04 00:00:02 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -59,7 +59,9 @@ class MusicLibraryController
     puts "Please enter the name of an artist:"
     my_artist = gets.strip
 
-    Artist.all.sort { |a, b| a.song <=> b.song }.each.with_index(1) { |s, i| puts "#{i}. #{s.song}" }
+    Artist.all.sort { |a, b| a.song <=> b.song }.each.with_index(1) do |a, i|
+      puts "#{i}. #{a.song}"
+    end
   end
 
   def list_songs_by_genre
